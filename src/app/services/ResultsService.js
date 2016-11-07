bd87fa83eb0e5e8f8941786547816d65b1da17a0
Nelson Amaral <noslenfa@gmail.com>
2016-11-07 18:43:08 +0000

    [img]updated screenshots for a smaller size; [others]updated README.MD file info;

--- src/app/services/ResultsService.js
+++ src/app/services/ResultsService.js
@@ -1,7 +1,7 @@
-import {API_REST_URL} from '../constants/Constants';
-import {API_ID} from '../constants/Constants';
-import {API_KEY} from '../constants/Constants';
-import RestService from './RestService'
+import {API_REST_URL} from "../constants/Constants";
+import {API_ID} from "../constants/Constants";
+import {API_KEY} from "../constants/Constants";
+import RestService from "./RestService"
 
 class ResultsService{
 
@@ -9,7 +9,7 @@ class ResultsService{
 //get all artists info based on input inserted
 	getAllSearchedArtistsInfo(artistName, pageNumber, cb, cberror){
 
-		let url = API_REST_URL + '/v1/artists?q=[artist-name:' + artistName + ']&appkey=' + API_KEY + '&appid=' + API_ID + '&limit=10&page=' + pageNumber;
+		let url = API_REST_URL + "/v1/artists?q=[artist-name:" + artistName + "]&appkey=" + API_KEY + "&appid=" + API_ID + "&limit=10&page=" + pageNumber;
 
 		RestService.get(url).then((response) => {
       cb(JSON.parse(response));
@@ -22,7 +22,7 @@ class ResultsService{
 //get artist info based on artist mkid
 	getArtistsInfo(mkid, cb, cberror){
 
-		let url = API_REST_URL + '/v1/artists/' + mkid + '/?appkey=' + API_KEY + '&appid=' + API_ID;
+		let url = API_REST_URL + "/v1/artists/" + mkid + "/?appkey=" + API_KEY + "&appid=" + API_ID;
 
 		RestService.get(url).then((response) => {
       cb(JSON.parse(response));
