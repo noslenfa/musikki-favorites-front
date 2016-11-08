88fd4ab6fbcd5b9afca13d8c58429c7fbaf3d646
Nelson Amaral <noslenfa@gmail.com>
2016-11-08 01:13:13 +0000

    [scss]updated styles related with responsavity issues; [js]updated constants to add new images for carousel; [img]removed and added new images for carousel;

--- src/app/constants/Constants.js
+++ src/app/constants/Constants.js
@@ -5,6 +5,7 @@ export const API_ID = "123456789";
 export const API_KEY = "123456789";
 export const logoUrl = "./app/images/musikki.svg"
 export const carouselInfo = [
-  {id: 1, imgCarouselUrl: "./app/images/carousel_01.png", slideLabel: "SEARCH", slideText: "Make some searches for your favorite artists!"},
-  {id: 2, imgCarouselUrl: "./app/images/carousel_02.png", slideLabel: "FAVORITES", slideText: "Check your favorite artists!"}
+  {id: 1, imgCarouselUrl: "./app/images/carousel_00.jpg", slideLabel: "FAVORITES", slideText: "Check your favorite music artists on your computer!"},
+  {id: 2, imgCarouselUrl: "./app/images/carousel_01.jpg", slideLabel: "SEARCH", slideText: "Search for new music artists on your mobile phone!"},
+  {id: 3, imgCarouselUrl: "./app/images/carousel_02.jpg", slideLabel: "LOGIN", slideText: "Login to all the music artists in yout tablet!"}
 ];
